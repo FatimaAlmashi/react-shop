619b9cc8948468c514003c5642a71d1049f6f517
Fatima Almashi <f.almashi@windowslive.com>
2019-04-04 17:23:34 +0300

    redux setup

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, { Component } from "react";
 // import logo from './logo.svg';
 import "./App.css";
 import Home from "./Components/Home";
+import Profile from "./Components/Profile";
 
 class App extends Component {
   render() {
     return (
       <div className="container">
-        <Home />
+        <Profile />
       </div>
     );
   }
